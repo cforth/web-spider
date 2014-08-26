51aa413f6df4ce1f08d86170d3b11fb863498ddb
chaif <cforth@163.com>
2014-08-26 14:23:37 +0800

    0.3版

--- urllib_stock.py
+++ urllib_stock.py
@@ -1,4 +1,4 @@
-#抓取股票关注 0.2版
+#抓取股票关注 0.3版
 #python3.4.1
 
 import urllib.request   #网络模块
@@ -21,6 +21,7 @@ def get_stock_info(url):
     price_target = re.findall(r'<span id="ctl04_txTgtPrice">(\d+\.\d+)</span>', str(text))      #正则匹配目标价
     grade = re.findall(r'<span id="ctl04_bgpj">([\u4e00-\u9fa5]+)</span>', str(text))           #正则匹配评级
     trade = re.findall(r'<span id="ctl04_lbHylbmc">([\u4e00-\u9fa5]+)</span>', str(text))       #正则匹配行业
+    trade = trade if len(trade) != 0 else '无'
     stock_date = re.findall(r'<span id="ctl04_lblzhxgrq" style="display:inline-block;width:90%;">(\d+\-\d+\-\d+)</span>', str(text))   #正则匹配日期
     
     if len(stock_id) != 0:
@@ -30,10 +31,10 @@ def get_stock_info(url):
     else:
         return ''
 
-def write_stock_file(mystr):
+def write_stock_file(mystr, file_name):
     """以写入方式打开文件，写入字符串mystr并自动关闭文件。
     """
-    with open('f:/temp/workfile.txt', 'w') as f:
+    with open(file_name, 'a') as f:
         f.write(str(mystr))
 
 
@@ -41,14 +42,20 @@ def main():
     """设置网页抓取地址头部、页面ID范围、地址尾部。
     合成需要抓取的网页地址，在ID范围内循环抓取数据存储在mystr中。
     最后写入文件。
-    """
+    """   
+    url_head = str(input(u'请输入网址前部：\n'))
+    url_tail = str(input(u'请输入网址后部：\n'))
+    start_id = int(input(u'请输入页面开始ID：\n'))
+    end_id = int(input(u'请输入页面结束ID：\n'))
+    file_name = 'f:/temp/' + str(input(u'请输入存入的文件名称:\n'))
     mystr = ''
-    url_head = 'hhh' #需替换
-    url_tail = 'ttt' #需替换
-    start_id = 0     #需替换
-    end_id = 10      #需替换
+    task_id = 1
+    task_all = end_id - start_id + 1
 
-    for i in range(end_id, start_id, -1):
-        mystr = mystr + get_stock_info(url_head + str(i) + url_tail)
+    for i in range(end_id, start_id - 1, -1):
+        print('任务进度 (%d/%d)' % (task_id, task_all))
+        mystr = get_stock_info(url_head + str(i) + url_tail)
+        write_stock_file(mystr, file_name)
+        task_id = task_id + 1
 
-    write_stock_file(mystr)
+    print('任务结束，数据存入文件： %s \n' % file_name)
