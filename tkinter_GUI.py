0125056fbca1afdb1d7e76f94ea21e264336ce9d
chaif <cforth@163.com>
2014-08-31 16:09:03 +0800

    Update tkinter_GUI.py

--- tkinter_GUI.py
+++ tkinter_GUI.py
@@ -3,19 +3,36 @@
 
 import tkinter as tk
 
-def resize(ev = None):
-    """scale的回调函数
-    修改labelDisplay的字体大小，当然其他信息也可以改，比如text
-    """
-    labelDisplay.config(font = 'Helvetica -%d bold' % scale.get())
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
 
+    def createWidgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.QUIT = tk.Button(self, text="QUIT", fg="red",
+                                            command=root.destroy)
+        self.QUIT.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
 
 root = tk.Tk()
-root.title('窗口标题')                #窗口标题
-root.geometry("250x150+0+0")         #窗口宽度与高度，位置
+root.title('我的窗口标题')
+root.geometry('300x200+0+0')  #窗口大小，位置
 
 #创建一个Label，是要放置在root内的，文本为'Hello World!'，还有字体与背景色  
-labelDisplay=tk.Label(root, text='Hello World!', font='Helvetica -12 bold',bg='red')
+labelDisplay=tk.Label(root, text='Hello World!', font='Helvetica -12 bold', bg='red')
+#将labelDisplay放置进root，如果没有这句话，则不会显示这个组件。
+labelDisplay.pack(fill= 'none', expand=1, ipadx=5, ipady=5)
 
-#将labelDisplay放置进root，如果没有这句话，则不会显示这个组件。fill, expand, ipadx, ipady的说明在下面 
-labelDisplay.pack(fill=None, expand=1, ipadx=5, ipady=5)
+labelDisplay1=tk.Label(root, text='Hello World!', font='Helvetica -12 bold', bg='green')  
+labelDisplay1.pack(fill='both', expand=1, padx=5, pady=5) 
+
+app = Application(master=root)
+app.mainloop()
