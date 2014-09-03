590e815279e2963857b85cb4851d5e42d4fc580b
chaif <cforth@163.com>
2014-09-03 16:25:39 +0800

    Update tkinter_GUI.py

--- tkinter_GUI.py
+++ tkinter_GUI.py
@@ -19,6 +19,17 @@ class App(Frame):
         self.label2 = Label(self, text='Hello World!', font='Helvetica -12 bold', bg='green')
         self.label2.pack(fill='none', expand=1, ipadx=100, ipady=20, padx=5, pady=5, side='top')
 
+        #创建文本框
+        self.text1=Entry()
+        self.text1.pack(fill='none', expand=1, ipadx=100, ipady=20, padx=5, pady=5, side='top')
+        self.contents = StringVar()
+        self.contents.set("hello")
+        self.text1["textvariable"] = self.contents
+        self.text1.bind("<Key-Return>",self.print_contents)#事件绑定
+        #self.text1.insert(INSERT,'AAAAA')#插入文本 
+        self.text1.config(font="Arial 10 bold")
+        #self.text1.config(state=DISABLED)#禁止用户编辑文本，当然也阻止了事件绑定
+
         #创建一个按钮，点击时运行say_hi
         self.hi_there = Button(self)
         self.hi_there['text'] = "Hello World\n(click me)"
@@ -32,6 +43,9 @@ class App(Frame):
     def say_hi(self):
         print('hi there, everyone!')
 
+    def print_contents(self, event):
+        print(self.contents.get())
+
 
 root = Tk()
 root.title('我的窗口标题')
@@ -42,3 +56,5 @@ root.minsize(500, 500)
 
 app = App(master=root)            #初始化窗口
 app.mainloop()                    #进入消息循环
+
+
