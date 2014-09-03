6a2dfda1f54940a10b7327ccd4e8d4c03f787b0a
chaif <cforth@163.com>
2014-09-03 14:49:56 +0800

    Update tkinter_GUI.py

--- tkinter_GUI.py
+++ tkinter_GUI.py
@@ -1,38 +1,44 @@
 # -*- coding: utf-8 -*-
 # Python3.4.1
 
-import tkinter as tk
+from tkinter import *
 
-class Application(tk.Frame):
+class App(Frame):
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
+        Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
 
     def createWidgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        #创建一个标签，设置文字及背景属性
+        self.label1 = Label(self, text='Hello World!', font='Helvetica -24 bold', fg='red', bg='blue')
+        #将标签放置在窗口中的位置
+        self.label1.pack(fill='none', expand=1, ipadx=500, ipady=20, padx=1, pady=1, side='top')
 
-        self.QUIT = tk.Button(self, text="QUIT", fg="red",
-                                            command=root.destroy)
-        self.QUIT.pack(side="bottom")
+        #创建第二个标签，同上
+        self.label2 = Label(self, text='Hello World!', font='Helvetica -12 bold', bg='green')
+        self.label2.pack(fill='none', expand=1, ipadx=100, ipady=20, padx=5, pady=5, side='top')
+
+        #创建一个按钮，点击时运行say_hi
+        self.hi_there = Button(self)
+        self.hi_there['text'] = "Hello World\n(click me)"
+        self.hi_there['command'] = self.say_hi
+        self.hi_there.pack(ipadx=100, ipady=20, padx=5, pady=5, side='top')
+
+        #创建退出按钮
+        self.QUIT = Button(self, text='QUIT', fg='red', command=root.destroy)
+        self.QUIT.pack(ipadx=120, ipady=20, padx=5, pady=5, side='bottom')
 
     def say_hi(self):
-        print("hi there, everyone!")
+        print('hi there, everyone!')
 
-root = tk.Tk()
+
+root = Tk()
 root.title('我的窗口标题')
-root.geometry('300x200+0+0')  #窗口大小，位置
+root.geometry('500x500+200+100')  #窗口大小，位置
+root.maxsize(1000, 1000)          #窗口最大尺寸
+root.minsize(500, 500)
 
-#创建一个Label，是要放置在root内的，文本为'Hello World!'，还有字体与背景色  
-labelDisplay=tk.Label(root, text='Hello World!', font='Helvetica -12 bold', bg='red')
-#将labelDisplay放置进root，如果没有这句话，则不会显示这个组件。
-labelDisplay.pack(fill= 'none', expand=1, ipadx=5, ipady=5)
 
-labelDisplay1=tk.Label(root, text='Hello World!', font='Helvetica -12 bold', bg='green')  
-labelDisplay1.pack(fill='both', expand=1, padx=5, pady=5) 
-
-app = Application(master=root)
-app.mainloop()
+app = App(master=root)            #初始化窗口
+app.mainloop()                    #进入消息循环
