b4627d0ff6087ef9c3dc9b667c54dee80d4717fb
chaif <cforth@163.com>
2014-09-04 14:43:16 +0800

    网络爬虫0.5版(窗口程序,增加了错误处理)

--- tkinter_GUI.py
+++ tkinter_GUI.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
+#网络爬虫0.5版
 # Python3.4.1
 
-from tkinter import *
+from tkinter import *   #Tk模块
 import urllib.request   #网络模块
 import re               #正则表达式模块
 import codecs           #文件编码转换
 import sys
 
+###############################################################
+#窗口模块开始
+
 class App(Frame):
+    """构建窗口程序
+    """
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
@@ -15,52 +21,56 @@ class App(Frame):
 
     def createWidgets(self):
         #创建一个标签，设置文字及背景属性
-        self.label1 = Label(self, text='网络爬虫（股票抓取）', font='Helvetica -24 bold', fg='red', bg='blue')
+        self.label = Label(self, text='网络爬虫（股票抓取）', font='Helvetica -24 bold', fg='snow', bg='tomato')
         #将标签放置在窗口中的位置
-        self.label1.pack(fill='none', expand=1, ipadx=500, ipady=20, padx=1, pady=1, side='top')
+        self.label.pack(fill='none', expand=1, ipadx=500, ipady=20, padx=1, pady=1, side='top')
 
         #创建网址前部文本框
+        self.label1 = Label(text='输入网页抓取网址前部分：', font='Helvetica -15 bold',)
+        self.label1.pack(fill='none', expand=1, ipadx=400,side='top')
         self.text1=Entry()
-        self.text1.pack(fill='none', expand=1, ipadx=400, ipady=5, padx=5, pady=5, side='top')
+        self.text1.pack(fill='none', expand=1, ipadx=100, ipady=5, padx=5, pady=5, side='top')
         self.contents = StringVar()
         self.contents.set("")
         self.text1["textvariable"] = self.contents
         self.text1.config(font="Arial 10 bold")
 
         #创建数字文本框
+        self.label2 = Label(text='输入起始序号：', font='Helvetica -15 bold',)
+        self.label2.pack(fill='none', expand=1, ipadx=400,side='top')
         self.text2=Entry()
-        self.text2.pack(fill='none', expand=1, ipadx=400, ipady=5, padx=5, pady=5, side='top')
+        self.text2.pack(fill='none', expand=1, ipadx=100, ipady=5, padx=5, pady=5, side='top')
         self.contents2 = StringVar()
         self.contents2.set("")
         self.text2["textvariable"] = self.contents2
         self.text2.config(font="Arial 10 bold")
 
+        self.label3 = Label(text='输入结束序号：', font='Helvetica -15 bold',)
+        self.label3.pack(fill='none', expand=1, ipadx=400,side='top')
         self.text3=Entry()
-        self.text3.pack(fill='none', expand=1, ipadx=400, ipady=5, padx=5, pady=5, side='top')
+        self.text3.pack(fill='none', expand=1, ipadx=100, ipady=5, padx=5, pady=5, side='top')
         self.contents3 = StringVar()
         self.contents3.set("")
         self.text3["textvariable"] = self.contents3
         self.text3.config(font="Arial 10 bold")
 
-        #创建一个按钮，点击时运行股票抓取
-        self.hi_there = Button(self)
+        #创建一个按钮，点击时运行网络爬虫
+        self.hi_there = Button()
         self.hi_there['text'] = "点击股票抓取"
         self.hi_there['command'] = self.do_stock
-        self.hi_there.pack(ipadx=100, ipady=20, padx=5, pady=5, side='top')
+        self.hi_there.pack(ipadx=50, ipady=10, padx=20, pady=10, side='left')
 
         #创建退出按钮
-        self.QUIT = Button(self, text='QUIT', fg='red', command=root.destroy)
-        self.QUIT.pack(ipadx=120, ipady=20, padx=5, pady=5, side='bottom')
-
-    def say_hi(self):
-        print('hi there, everyone!')
-
-    def print_contents(self, event):
-        print(self.contents.get())
+        self.QUIT = Button(text='QUIT', fg='red', command=root.destroy)
+        self.QUIT.pack(ipadx=60, ipady=10, padx=20, pady=10, side='right')
 
     def do_stock(self):
-        spider_go(str(self.contents.get()), int(self.contents2.get()), int(self.contents3.get()))
-        remove_r('new.txt','old.txt','save.txt')  
+        #运行网络爬虫
+        spider_go(str(self.contents.get()), str(self.contents2.get()), str(self.contents3.get()))
+        #可选，删除之前已获取股票数据中的重复行
+        #remove_r('new.txt','old.txt','save.txt')
+
+#窗口模块结束
 
 ##############################################################
 ##网络爬虫模块开始
@@ -73,8 +83,8 @@ def get_stock_info(url):
     user_agent = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.6 Safari/537.36'
     req = urllib.request.Request(url)
     req.add_header('User-Agent', user_agent)
-    
-    response = urllib.request.urlopen(req)  #抓取网页
+
+    response = urllib.request.urlopen(req)  #抓取网页      
     text = response.read().decode('gb2312', 'ignore')
     
     stock_id = re.findall(r'\d+\.S[ZH]', str(text))                                             #正则匹配股票代码
@@ -99,21 +109,36 @@ def write_stock_file(mystr, file_name):
         f.write(str(mystr))
 
 
-def spider_go(url_head, start_id, end_id):
+def spider_go(url_head, start, end):
     """设置网页抓取地址头部、页面ID范围。
     合成需要抓取的网页地址，在ID范围内循环抓取数据存储在mystr中。
     最后写入文件。
     工作目录为'F:/temp/'。
     """   
+    try:
+        start_id = int(start)
+        end_id = int(end)
+    except:
+        print('序号输入有误！终止任务！必须输入数字！')
+        return
+
+    if end_id < start_id:
+        print('序号输入有误！终止任务！起始序号必须小于或等于结束序号！')
+        return
+        
     file_name = 'f:/temp/new.txt'
     mystr = ''
     task_id = 1
     task_all = end_id - start_id + 1
-
+    
     for i in range(end_id, start_id - 1, -1):
-        print('任务进度 ({0}/{1})'.format(task_id, task_all))
-        mystr = get_stock_info(url_head + str(i))
+        try:
+            mystr = get_stock_info(url_head + str(i))
+        except:
+            print('网页获取失败！终止任务！错误信息：{0}'.format(sys.exc_info()[0]))
+            return
         write_stock_file(mystr, file_name)
+        print('任务进度 ({0}/{1})'.format(task_id, task_all))
         task_id = task_id + 1
 
     print('任务结束，数据存入文件： {0} \n'.format(file_name))
@@ -142,18 +167,24 @@ def remove_r(file_new, file_old, file_save):
     print('任务结束，去重后的数据存入{0}\n'.format(file_save))
     
 ##网络爬虫模块结束
+
 ####################################################################
-
-
+#主循环
 
 root = Tk()
-root.title('网络爬虫0.5版')
-root.geometry('500x500+200+100')  #窗口大小，位置
-root.maxsize(1000, 1000)          #窗口最大尺寸
-root.minsize(500, 500)
+    
+def main():
+    root.title('网络爬虫0.5版')
+    root.geometry('500x500+200+100')  #窗口大小，位置
+    root.maxsize(1000, 1000)          #窗口最大尺寸
+    root.minsize(500, 500)
 
 
-app = App(master=root)            #初始化窗口
-app.mainloop()                    #进入消息循环
+    app = App(master=root)            #初始化窗口
+    app.mainloop()                    #进入消息循环
 
 
+if __name__ == '__main__':
+    main()
+
+#End
