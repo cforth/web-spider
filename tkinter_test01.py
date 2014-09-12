67bde5b3c3732476ac6fa1fbde32e192c161880b
cforth <cforth@163.com>
2014-09-12 13:11:55 +0800

    删除临时代码

--- tkinter_test01.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-def on_click():
-    button['text'] = 'It is changed.'
-root = Tk(className='aaa')
-button = Button(root)
-button['text'] = 'change it'
-button['command'] = on_click    #事件关联函数
-button.pack()
-root.mainloop()
--- urllib2_test01.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8  
-
-import urllib2  
-
-url = 'http://cfishacker.com/stock/'  
-response = urllib2.urlopen(url)  #抓取网页内容
-the_page = response.read()  
-print the_page  
