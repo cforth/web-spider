ec9a02d449acc446c6b2ef778fb3925d4e68b73f
chaif <cforth@163.com>
2014-09-13 16:38:24 +0800

    删除原网页中所有多余空白字符与回车键后再匹配

--- spider.py
+++ spider.py
@@ -6,6 +6,7 @@
 
 import urllib2
 import urllib
+import chardet    #检测网页编码
 import re
 
 def get_url_info(url):
@@ -17,16 +18,20 @@ def get_url_info(url):
     req = urllib2.Request(url)
     req.add_header('User-Agent', user_agent)
 
-    response = urllib2.urlopen(req)          #抓取网页      
-    return response.read().decode('gb2312')  #根据网页编码修改
+    data = urllib2.urlopen(req).read()            #抓取网页
+    html_code = chardet.detect(data)              #通过chardet模块检测编码
+    return data.decode(html_code['encoding'])     
 
 
 def match_text(text, rules):
     """根据正则表达式匹配目标文本
     text为被分析的文本字符串。
+    text去除每行行首与行尾空白字符、空行
     rules为一个字典，键为匹配目标名称，值为匹配规则（正则表达式字符串）。
     函数返回一个字典，键为匹配目标名称，值为符合匹配规则的字符串列表。
     """
+    text = re.sub('\s*\n\s*','', text)            #去除每行行首与行尾空白字符、空行
+    
     find_dict = {}
     
     for key in rules :
@@ -40,13 +45,11 @@ class Spider():
     """网页爬虫类
     从标准输入获取网址url_head、起始页序号start、终止页序号end
     """
-    def __init__(self, rules):
+    def __init__(self, rules, url_head, start, end):
         self.rules = rules
-        self.url_head = str(raw_input(u'请输入网址前部：\n'))
-        self.start = int(raw_input(u'请输入页面开始ID：\n'))
-        self.end = int(raw_input(u'请输入页面结束ID：\n'))
-    
-
+        self.url_head = url_head
+        self.start = start
+        self.end = end    
         self.finds = {}
 
     def catch(self):
@@ -62,15 +65,25 @@ class Spider():
 
 
 def main():
-    stock_rules = {'stock_id':'\d+\.S[ZH]',                                         #正则匹配股票代码
-             'price_inid':'<span id="ctl04_lbSpj">(\d+\.\d+)</span>',               #正则匹配初始价
-             'price_target':'<span id="ctl04_txTgtPrice">(\d+\.\d+)</span>',        #正则匹配目标价
-             'grade':u'<span id="ctl04_bgpj">([\u4e00-\u9fa5]+)</span>',            #正则匹配评级
-             'trade':u'<span id="ctl04_lbHylbmc">([\u4e00-\u9fa5]+)</span>',        #正则匹配行业
-             'stock_date':'<span id="ctl04_lblzhxgrq" style="display:inline-block;width:90%;">(\d+\-\d+\-\d+)</span>'}  #正则匹配日期    
-    stock_spider = Spider(stock_rules)
-    stock_spider.catch()
-    print stock_spider.finds
+##    stock_rules = {'stock_id':'\d+\.S[ZH]',                                         #正则匹配股票代码
+##             'price_inid':'<span id="ctl04_lbSpj">(\d+\.\d+)</span>',               #正则匹配初始价
+##             'price_target':'<span id="ctl04_txTgtPrice">(\d+\.\d+)</span>',        #正则匹配目标价
+##             'grade':u'<span id="ctl04_bgpj">([\u4e00-\u9fa5]+)</span>',            #正则匹配评级
+##             'trade':u'<span id="ctl04_lbHylbmc">([\u4e00-\u9fa5]+)</span>',        #正则匹配行业
+##             'stock_date':'<span id="ctl04_lblzhxgrq" style="display:inline-block;width:90%;">(\d+\-\d+\-\d+)</span>'}  #正则匹配日期
+##    url_head = str(raw_input(u'请输入网址前部：\n'))
+##    start = int(raw_input(u'请输入页面开始ID：\n'))
+##    end = int(raw_input(u'请输入页面结束ID：\n'))
+    
+
+    book_rules = {'title':u'<title>(.*)</title>'}
+    url_head = 'http://book.douban.com/subject/'
+    start = 4866934
+    end = 4866940
+    
+    my_spider = Spider(book_rules, url_head, start, end)
+    my_spider.catch()
+    print my_spider.finds
 
 if __name__ == '__main__' :
     main()
