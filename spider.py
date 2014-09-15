d3d31d22b1d77fc00e9bfb96070b19024b831757
chaif <cforth@163.com>
2014-09-15 14:07:42 +0800

    改进Spider类，提高抓取速度

--- spider.py
+++ spider.py
@@ -19,7 +19,7 @@ def write_file(mystr, file_name):
 
 def get_url_info(url):
     """抓取网页内容
-    网页请求头部为Chrome信息，被抓取网页的编码为gb2312。
+    网页请求头部为Chrome信息，通过chardet模块检测网页编码。
     返回包含网页内容的字符串。
     """
     user_agent = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.6 Safari/537.36'
@@ -51,50 +51,40 @@ def match_text(text, rules):
 
 class Spider():
     """网页爬虫类
-    从标准输入获取网址url_head、起始页序号start、终止页序号end
     """
-    def __init__(self, rules, url_head, start, end):
+    def __init__(self):
+        self.rules = {}
+        self.url = ''
+
+    def set_rules(self, rules):
+        """设置网页内容抓取规则
+        """
         self.rules = rules
-        self.url_head = url_head
-        self.start = start
-        self.end = end    
-        self.finds = {}
 
-    def format_finds(self):
-        """格式化抓取结果
-        顺序排序后的finds_list为一个元素为元组的列表。
+    def set_url(self, url):
+        """设置需要抓取的网址
         """
-        finds_str = ''
-        finds_list = sorted(self.finds.items(), key=lambda d:d[0])
-        for lst in finds_list :
-            finds_str += lst[0] + ':\n'
-            for key, value in lst[1].items() :
-                finds_str +=  '  ' + key + ' :\n'
-                for v in value:
-                    finds_str +=  '    ' + v + '\n'
-            finds_str += '\n'
-        return finds_str
-
-    def save_finds(self, file_name) :
-        """保存格式化后的抓取结果到文件
-        """
-        write_file(self.format_finds(), file_name)
+        self.url = url
 
     def catch(self):
-        """根据匹配规则批量抓取一些网页
-        顺序为页码从小到大，函数返回一个包含所有匹配目标信息的字典。
+        """根据匹配规则抓取网页
+        返回匹配结果格式化后的字符串。
         """
-        page = self.start
-        for i in range(self.start, self.end + 1, 1):
-            text = get_url_info(self.url_head + str(i))
-            find_dict = match_text(text, self.rules)
-            self.finds[str(page)] = find_dict
-            page += 1
-        return self.finds
+        text = get_url_info(self.url)
+        finds_dict = match_text(text, self.rules)
+        
+        finds_str = self.url + ' :\n'
+        for key, value in finds_dict.items() :
+            finds_str += '  ' + key + ' :\n'
+            for v in value:
+                    finds_str +=  '    ' + v + '\n'
+            finds_str += '\n'
+            
+        return finds_str + '\n\n'
 
 
 def main():
-##    stock_rules = {'stock_id':'\d+\.S[ZH]',                                         #正则匹配股票代码
+##    rules = {'stock_id':'\d+\.S[ZH]',                                         #正则匹配股票代码
 ##             'price_inid':'<span id="ctl04_lbSpj">(\d+\.\d+)</span>',               #正则匹配初始价
 ##             'price_target':'<span id="ctl04_txTgtPrice">(\d+\.\d+)</span>',        #正则匹配目标价
 ##             'grade':u'<span id="ctl04_bgpj">([\u4e00-\u9fa5]+)</span>',            #正则匹配评级
@@ -105,19 +95,27 @@ def main():
 ##    end = int(raw_input(u'请输入页面结束ID：\n'))
     
 
-    book_rules = {'title':u'<title>(.*)</title>',
+    rules = {'title':u'<title>(.*)</title>',
                   'keywords':u'<meta name="keywords" content="(.*?)">', #不贪婪匹配
                   'intro':u'<div class="intro"><p>(.*?)</p>'}           #不贪婪匹配
     url_head = 'http://book.douban.com/subject/'
-
-    
+   
     start = 4866934
-    end = start + 9
-    for i in range(3) :
-        my_spider = Spider(book_rules, url_head, start, end)
-        my_spider.catch()
-        my_spider.save_finds('f:/111.txt')
-        start, end = end + 1, end + 10
+    end = 4866943
+    
+    my_spider = Spider()
+    my_spider.set_rules(rules)
+    file_name = 'f:/111.txt'
+
+    all_page = end - start + 1
+    now_page = 0
+    for page in range(start, end + 1, 1) :
+        my_spider.set_url(url_head + str(page))
+        write_file(my_spider.catch(), file_name)
+        now_page += 1
+        print u'页面抓取中(%s/%s)' % (now_page, all_page)
+
+    print u'全部任务结束！抓取结果保存在 %s' % file_name
 
 if __name__ == '__main__' :
     main()
