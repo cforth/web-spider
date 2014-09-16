a2e63e626d6209a86a5c5bc029fc5c42eb82e559
chaif <cforth@163.com>
2014-09-16 16:57:35 +0800

    回退到上个版本，不分批次保存结果

--- spider.py
+++ spider.py
@@ -8,18 +8,10 @@ import urllib2
 import urllib
 import chardet    #检测网页编码
 import re
-import codecs
-
-def write_file(mystr, file_name):
-    """以写入方式打开文件，默认文件字符编码为utf-8，写入字符串mystr并自动关闭文件。
-    """
-    with codecs.open(file_name, 'a', 'utf-8') as f:
-        f.write(mystr)
-
 
 def get_url_info(url):
     """抓取网页内容
-    网页请求头部为Chrome信息，通过chardet模块检测网页编码。
+    网页请求头部为Chrome信息，被抓取网页的编码为gb2312。
     返回包含网页内容的字符串。
     """
     user_agent = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.6 Safari/537.36'
@@ -51,40 +43,44 @@ def match_text(text, rules):
 
 class Spider():
     """网页爬虫类
+    从标准输入获取网址url_head、起始页序号start、终止页序号end
     """
-    def __init__(self):
-        self.rules = {}
-        self.url = ''
-
-    def set_rules(self, rules):
-        """设置网页内容抓取规则
-        """
+    def __init__(self, rules, url_head, start, end):
         self.rules = rules
+        self.url_head = url_head
+        self.start = start
+        self.end = end    
+        self.finds = {}
 
-    def set_url(self, url):
-        """设置需要抓取的网址
+    def print_finds(self):
+        """格式化输出抓取结果
+        逆向排序后的finds_list为一个元素为元组的列表。
         """
-        self.url = url
+        finds_list = sorted(self.finds.items(), key=lambda d:d[0], reverse = True)
+        for lst in finds_list :
+            print lst[0] + ':'
+            for key, value in lst[1].items() :
+                print '  ' + key + ' :'
+                for v in value:
+                    print '    ' + v
+            print '\n'
+                
 
     def catch(self):
-        """根据匹配规则抓取网页
-        返回匹配结果格式化后的字符串。
+        """根据匹配规则批量抓取一些网页
+        顺序为页码从大到小，函数返回一个包含所有匹配目标信息的字典。
         """
-        text = get_url_info(self.url)
-        finds_dict = match_text(text, self.rules)
-        
-        finds_str = self.url + ' :\n'
-        for key, value in finds_dict.items() :
-            finds_str += '  ' + key + ' :\n'
-            for v in value:
-                    finds_str +=  '    ' + v + '\n'
-            finds_str += '\n'
-            
-        return finds_str + '\n\n'
+        page = self.end
+        for i in range(self.end, self.start - 1, -1):
+            text = get_url_info(self.url_head + str(i))
+            find_dict = match_text(text, self.rules)
+            self.finds[str(page)] = find_dict
+            page = page - 1
+        return self.finds
 
 
 def main():
-##    rules = {'stock_id':'\d+\.S[ZH]',                                         #正则匹配股票代码
+##    stock_rules = {'stock_id':'\d+\.S[ZH]',                                         #正则匹配股票代码
 ##             'price_inid':'<span id="ctl04_lbSpj">(\d+\.\d+)</span>',               #正则匹配初始价
 ##             'price_target':'<span id="ctl04_txTgtPrice">(\d+\.\d+)</span>',        #正则匹配目标价
 ##             'grade':u'<span id="ctl04_bgpj">([\u4e00-\u9fa5]+)</span>',            #正则匹配评级
@@ -95,27 +91,16 @@ def main():
 ##    end = int(raw_input(u'请输入页面结束ID：\n'))
     
 
-    rules = {'title':u'<title>(.*)</title>',
+    book_rules = {'title':u'<title>(.*)</title>',
                   'keywords':u'<meta name="keywords" content="(.*?)">', #不贪婪匹配
                   'intro':u'<div class="intro"><p>(.*?)</p>'}           #不贪婪匹配
     url_head = 'http://book.douban.com/subject/'
-   
     start = 4866934
     end = 4866943
     
-    my_spider = Spider()
-    my_spider.set_rules(rules)
-    file_name = 'f:/111.txt'
-
-    all_page = end - start + 1
-    now_page = 0
-    for page in range(start, end + 1, 1) :
-        my_spider.set_url(url_head + str(page))
-        write_file(my_spider.catch(), file_name)
-        now_page += 1
-        print u'页面抓取中(%s/%s)' % (now_page, all_page)
-
-    print u'全部任务结束！抓取结果保存在 %s' % file_name
+    my_spider = Spider(book_rules, url_head, start, end)
+    my_spider.catch()
+    my_spider.print_finds()
 
 if __name__ == '__main__' :
     main()
